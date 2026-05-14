62e8b447964738a818dc49b336e3d189b858d5e6
Md-razvi <48431475+Md-razvi@users.noreply.github.com>
2026-05-14 16:13:06 +0530

    second commit

---
+++ src/main/java/InterviewIQ/AI_project/AiProjectApplication.java
@@ -0,0 +1,13 @@
+package InterviewIQ.AI_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiProjectApplication.class, args);
+	}
+
+}
